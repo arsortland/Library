0e58e7d9441a44edf540df365d42031a187a8a5b
André Sortland <arsortland@gmail.com>
2022-01-17 15:26:47 +0100

    added modal and form that sends values to js.script

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-/**
- * MAKE HTML
- * MAKE A LIL CSS TO GET THE SITE RIGHT
- * GET JS AFTER THAT 
- * AND LASTLY STYLE HTML AND CSS
-*/
+const modal = document.querySelector('.modal');
+const btn = document.getElementById('addbtn')
+const closeIt = document.querySelector('.close');
 
-let myLib = [];
+const form = document.querySelector('.form');
+
+const title = document.querySelector('.title');
+const author = document.querySelector('.author');
+const pages = document.querySelector('.pages');
+const readbox = document.querySelector('#readbox');
+const info = document.querySelector('.info');
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+window.onclick = function (e) {
+    if (e.target == modal){
+        modal.style.display = "none";
+    }
+}
+
+
+const myLib = [];
 
 function Book(title, author, pages, read, info = ""){
     this.title = title;
@@ -15,25 +31,27 @@ function Book(title, author, pages, read, info = ""){
     this.info = info;
 }
 
+const test = new Book('enTittel', 'enForfatter', 500, 'true', 'much wow')
+const test2 = new Book('annenTittel', 'annenForfatter', 999, 'false')
 
 
 function addBookToLib(){
-    const popup = document.getElementById('popup');
-    popup.classList.toggle('hide');
-    popup.classList.toggle('show');
-    // toggle in another class for body/html to make background more grey?
+    let aTitle = title.value;
+    let anAuthor = author.value; 
+    let totPages = pages.value;
+    let haveRead = readbox.checked;
+    let haveInfo = info.value;
+    const newBook = new Book(aTitle, anAuthor, totPages, haveRead, haveInfo);
+    console.log(title.value);
+    console.log(author.value);
+    console.log(pages.value);
+    console.log(readbox.checked);
+    console.log(info.value);
+    myLib.push(test);
+    myLib.push(test2);
+    myLib.push(newBook);
+    //reset form and close modal here or make a func that does. MAKE FUNC THAT DOES YOU!
 }
 
-function showBooks(){
-    for (let i=0;i<myLib.length;i++){
-        aBook.append(myLib[i]);
-        //myLib[i] <- Make this append to some DOM and show it!
-    }
-}
-
-//write func that loops through arr of books and displays them on a table of kind.
-//Add a new book button for making a new book obj.
-const libcontainer = document.querySelector('.libcontainer');
-const aBook = document.createElement('div');
-aBook.classList.add('abook');
-libcontainer.appendChild(aBook);
+// //write func that loops through arr of books and displays them on a table of kind.
+// //Add a new book button for making a new book obj.
